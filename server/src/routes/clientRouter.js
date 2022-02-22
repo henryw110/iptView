15d01d434c831602a78788864bc2510eb9b47117
Henry Wiest <henrywiest@macbook-air.myfiosgateway.com>
2022-02-22 17:02:23 -0500

    heroku fix?

--- server/src/routes/clientRouter.js
+++ server/src/routes/clientRouter.js
@@ -2,24 +2,27 @@ import express from "express";
 import getClientIndexPath from "../config/getClientIndexPath.js";
 import pkg from "forge-apis";
 const { BucketsApi, ObjectsApi, PostBucketsPayload } = pkg
-import  oauth  from'./common/oauth.js';
+import oauth from './common/oauth.js';
 const { getClient, getInternalToken } = oauth;
 import config from "../config.js"
 
 const router = new express.Router();
-const clientRoutes = ["/", 
-"/user-sessions/new",
- "/users/new",
-"/model/:id"];
+const clientRoutes = ["/",
+  "/user-sessions/new",
+  "/users/new",
+  "/model/:id"];
 
 router.use(async (req, res, next) => {
-  try {const token = await getInternalToken();}
-  catch(err) {
+  try {
+    const token = await getInternalToken();
+    req.oauth_token = token;
+    req.oauth_client = getClient();
+    next();
+  }
+  catch (err) {
     console.log(err)
   }
-  req.oauth_token = token;
-  req.oauth_client = getClient();
-  next();
+
 });
 
 
